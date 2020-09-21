223206e6127e58be88560c561c1148e7af561d5e
Pablo Velez <pablo.velez.p@gmail.com>
2020-09-21 18:55:27 -0500

    vue test

---
+++ src/components/List.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+     <h1>Lista</h1>
+
+     <div v-bind:key="item.name" v-for="item in list">
+       <TodoItem v-bind:item="item" v-on:blueItem="$emit('blueish',item.name)"   />
+     </div>
+
+
+</div>
+</template>
+
+<script >
+
+import TodoItem from './TodoItem.vue'
+
+export default {
+    name: 'List',
+    components: {
+        TodoItem
+    },
+    props: ['list'],
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,40 @@
+<template>
+    <div 
+    class="item"
+    v-on:click="blueish"
+   >
+   <p
+    class="todo-item" v-bind:class="{'is-clicked':item.clicked}"
+   >{{item.name}} tiene {{item.length}} productos - su mayor producto es {{item.products[0].name}} y su precio: {{item.products[0].price}} </p>
+    </div>
+</template>
+
+<script >
+   export default {
+       name: "TodoItem",
+       props:["item"],
+       methods: {
+           blueish() {
+              this.item.clicked = !this.item.clicked
+           }
+       }
+       } 
+</script>
+
+<style scoped>
+.item {
+    cursor: pointer;
+}
+
+.todo-item {
+    background: #f4f4f4;
+    padding:10px;
+    border-bottom: 1px #ccc dotted;
+    margin-bottom: 6px;
+    box-shadow: 1px 2px 0px 0px rgba(0,0,0,0.2)
+}
+
+.is-clicked {
+    color: blue;
+    }
+</style>
